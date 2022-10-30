d29281aa597a50d2df2f7c60f16ce2f1ad3be6ab
RPK <invertdna@gmail.com>
2022-10-29 21:31:15 -0700

    timeseries w reasonable gamma estimating construction effect

--- Scripts/timeseries_model/PlotTimeSeries.R
+++ Scripts/timeseries_model/PlotTimeSeries.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(here)
 
 ####
-stanMod <- readRDS(here("Scripts/timeseries_model/modelFit_20221009.RDS"))
+stanMod <- readRDS(here("Scripts/timeseries_model/modelFit_20221028.RDS"))
 
 f <- stanMod[3][[1]]  #data in
 s <- stanMod[[2]] #fitted model itself
@@ -24,7 +24,7 @@ resOut <- expand_grid(time_idx = 1:length(unique(f$time_idx)),
 
 #plot means
 resOut %>% 
-  filter(species_idx == 1) %>% 
+  filter(species_idx == 8) %>% 
   ggplot(aes(x = time_idx, y = log(meandnaconc))) +
   geom_point() +
   geom_point(aes(x = time_idx, y = mean_est), color = "red") +
@@ -84,14 +84,14 @@ for (i in 1:length(unique(f$species))){
                                     creek_idx == 2 ~ "Chuckanut",
                                     creek_idx == 3 ~ "Padden",
                                     creek_idx == 4 ~ "Squalicum")) %>%
-       mutate(species = case_when(species_idx == 1 ~ "Oncorhynchus clarkii",
-                                  species_idx == 2 ~ "Oncorhynchus kisutch",
-                                  species_idx == 3 ~ "Oncorhynchus mykiss")) %>%  ##to fix NAs in unobserved samples
+       # mutate(species = case_when(species_idx == 1 ~ "Oncorhynchus clarkii",
+                                  # species_idx == 2 ~ "Oncorhynchus kisutch",
+                                  # species_idx == 3 ~ "Oncorhynchus mykiss")) %>%  ##to fix NAs in unobserved samples
        filter(species_idx == i) %>%
        ggplot(aes(x = month, y = log(meandnaconc))) +
        geom_point() +
        geom_point(aes(x = month, y = mean_est), color = "red", size = 1.5, alpha = .5) +
-       geom_segment(aes(x = month, xend = month, y = pp_05, yend = pp_975), color = "red", size = .3, alpha = .5) +
+       geom_segment(aes(x = month, xend = month, y = pp_025, yend = pp_975), color = "red", size = .3, alpha = .5) +
        geom_segment(aes(x = month, xend = month, y = pp_25, yend = pp_75), color = "red", size = .7, alpha = .5) +
        facet_grid(~creekname ~station) +
        ggtitle(paste0(unique(f$species)[i], "\nPosterior Predictive Check \n(predicted mean +/- 95 CI)"))
@@ -101,7 +101,14 @@ for (i in 1:length(unique(f$species))){
 p1
 p2
 p3
+p4
+p5
+p6
 
+f %>% 
+  filter(species == "Oncorhynchus tshawytscha") %>% 
+  filter(creek == "3Chk") %>% 
+  as.data.frame()
 
 ##One way to plot effect of culverts:
 f %>%
@@ -118,7 +125,7 @@ f %>%
   mutate(species = case_when(species_idx == 1 ~ "Oncorhynchus clarkii",
                              species_idx == 2 ~ "Oncorhynchus kisutch",
                              species_idx == 3 ~ "Oncorhynchus mykiss")) %>%  ##to fix NAs in unobserved samples
-  filter(species_idx == 3) %>%
+  filter(species_idx == 1) %>%
   ggplot(aes(x = as.numeric(month), y = log(meandnaconc))) +
   # geom_point() +
   geom_point(aes(x = as.numeric(month), y = mean_est, color = station), size = 1.5, alpha = .5) +
@@ -129,90 +136,53 @@ f %>%
   ggtitle("Posterior Predictive Check \n(predicted mean +/- 95 CI)")
 
 
-###CONSTRUCTION Causal Analysis Plot:
 
 
 
-fit_nc <- readRDS(here("Scripts/timeseries_model/modelFit_20221009_noConstruction.RDS"))
+gamma_means <- expand_grid(time_idx = 2:(length(unique(f$time_idx))),
+                           species_idx = 1:length(unique(f$species_idx)),
+                           #creek_idx = 1:length(unique(f$creek)),
+                           #station_idx = 1:length(unique(f$station_idx)),
+                           construction_idx = 1:length(unique(f$construction_idx))
+                           ) %>% 
+  mutate(gamma_mean = NA,
+         gamma_025 = NA,
+         gamma_975 = NA)
 
-f1 <- fit_nc[3][[1]]  #data in
-s1 <- fit_nc[[2]] #fitted model itself
-
-resOutNoConstr <- expand_grid(time_idx = 1:length(unique(f1$time_idx)),
-                              station_idx = 1:length(unique(f1$station_idx)),
-                              species_idx = 1:length(unique(f1$species_idx)),
-                              creek_idx = 1:length(unique(f1$creek_idx))) %>% 
-  mutate(mean_est = rstan::summary(s1, par = "mu")$summary[,1],
-         ci25 = rstan::summary(s1, par = "mu")$summary[,5],
-         ci75 = rstan::summary(s1, par = "mu")$summary[,7]) %>% 
-  left_join(f1) %>% 
-  left_join(data.frame(sigma_dna = rstan::summary(s1, par = "sigma_dna")$summary[,1], 
-                       species_idx = 1:1:length(unique(f1$species_idx))))
-
-ppOutNoConstr <- expand_grid(time_idx = 1:length(unique(f1$time_idx)),
-                             station_idx = 1:length(unique(f1$station_idx)),
-                             species_idx = 1:length(unique(f1$species_idx)),
-                             creek_idx = 1:length(unique(f1$creek_idx)))
-ppOutNoConstr[,5:11] <- 0.0
-colnames(ppOutNoConstr)[5:11] <- paste0("pp_", c("025", "05", 25, 50, 75, 90, 975))
-
-for (i in 1:nrow(ppOutNoConstr)){
-  ppOutNoConstr[i,5:11] <- as.list(getPostPred(s1, 
-                                               ppOutNoConstr$time_idx[i], 
-                                               ppOutNoConstr$station_idx[i], 
-                                               ppOutNoConstr$species_idx[i], 
-                                               ppOutNoConstr$creek_idx[i]))
+for (i in 1:nrow(gamma_means)){
+  a <- paste0("gamma[", 
+              gamma_means$time_idx[i]-1, ",",
+              gamma_means$species_idx[i], ",",
+              #gamma_means$creek_idx[i], ",",
+              #gamma_means$station_idx[i], ",",
+              gamma_means$construction_idx[i], "]")
+  
+  gamma_means$gamma_mean[i] <- rstan::summary(s, par = a)$summary[,1]
+  gamma_means$gamma_025[i] <- rstan::summary(s, par = a)$summary[,4]
+  gamma_means$gamma_975[i] <- rstan::summary(s, par = a)$summary[,8]
 }
 
+# gamma_means %>% 
+#   mutate(month = time_idx) %>% 
+#   filter(construction_idx == 2 & time_idx > 0 & species_idx < 4) %>% 
+#   ggplot(aes(x = as.factor(time_idx), 
+#              y = gamma_mean)) +
+#     geom_point() +
+#     geom_segment(aes(x = time_idx, xend = time_idx, y = gamma_025, yend = gamma_975)) +
+#     facet_grid(~species_idx) +
+#     xlab("time_idx") + ylab("Gamma")
+  
+gamma_means <- f %>% select(time, time_idx) %>%
+  distinct() %>%
+  mutate(month = ifelse(time_idx < 11, time_idx + 2, time_idx-10)) %>% 
+  left_join(gamma_means)
 
-expected <- f1 %>%
-  mutate(logY = log(meandnaconc)) %>%
-  right_join(resOutNoConstr) %>%
-  mutate(month = ifelse(time_idx < 11, time_idx + 2, time_idx - 10),
-         month = as.factor(month)) %>% 
-  left_join(ppOutNoConstr) %>% 
-  mutate(station = ifelse(station_idx == 1, "Downstream", "Upstream")) %>%
-  mutate(creekname = case_when(creek_idx == 1 ~ "Portage",
-                               creek_idx == 2 ~ "Chuckanut",
-                               creek_idx == 3 ~ "Padden",
-                               creek_idx == 4 ~ "Squalicum")) %>%
-  mutate(species = case_when(species_idx == 1 ~ "Oncorhynchus clarkii",
-                             species_idx == 2 ~ "Oncorhynchus kisutch",
-                             species_idx == 3 ~ "Oncorhynchus mykiss")) %>% 
-  filter(creek_idx == 3 & month %in% c(8,9,10,11,12))
 
-observed <- f %>% 
-  arrange(time_idx, creek_idx) %>% 
-  ungroup() %>% 
-  mutate(logY = log(meandnaconc)) %>%
-  right_join(resOut) %>%
-  mutate(month = ifelse(time_idx < 11, time_idx + 2, time_idx - 10),
-         month = as.factor(month)) %>% 
-  left_join(ppOut) %>% 
-  mutate(station = ifelse(station_idx == 1, "Downstream", "Upstream")) %>%
-  mutate(creekname = case_when(creek_idx == 1 ~ "Portage",
-                               creek_idx == 2 ~ "Chuckanut",
-                               creek_idx == 3 ~ "Padden",
-                               creek_idx == 4 ~ "Squalicum")) %>%
-  mutate(species = case_when(species_idx == 1 ~ "Oncorhynchus clarkii",
-                             species_idx == 2 ~ "Oncorhynchus kisutch",
-                             species_idx == 3 ~ "Oncorhynchus mykiss")) %>% 
-  filter(creek_idx == 3 & month %in% c(8,9,10,11,12))
+gamma_means %>% 
+  filter(construction_idx == 2, species_idx < 6 & month %in% c(1:12)) %>% 
+  ggplot(aes(x = as.factor(month), y = gamma_mean)) +
+    geom_point() +
+    geom_segment(aes(x = as.factor(month), xend = as.factor(month), y = gamma_025, yend = gamma_975)) +
+    facet_grid(.~species_idx)
 
-observed <- observed %>% 
-  mutate(state = "observed")
-expected <- expected %>% 
-  mutate(state = "expected")
 
-(ConstrEffect <- 
-  observed %>% 
-    bind_rows(expected) %>% 
-    #filter(species_idx == 3) %>%
-    ggplot(aes(x = month, y = log(meandnaconc), color = state)) +
-    # geom_jitter(width = 0.05) +
-    geom_point(aes(x = month, y = mean_est, color = state), size = 1.5, alpha = .5) +
-    #geom_segment(aes(x = month, xend = month, y = pp_05, yend = pp_975, color = state), size = .3, alpha = .5) +
-    geom_segment(aes(x = month, xend = month, y = pp_25, yend = pp_75, color = state), size = .7, alpha = .5) +
-    facet_grid(~species ~station))
-
-ggsave(ConstrEffect)
--- Scripts/timeseries_model/Run_TimeSeries.R
+++ Scripts/timeseries_model/Run_TimeSeries.R
@@ -6,9 +6,10 @@ library(rstan)
 library(loo)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
+library(shinystan)
 
 
-d <-readRDS(here("Output/salmonids_abs_abundance_bio_posterior_rpk.RDS")) %>% 
+d <-readRDS(here("Output/salmonids_abs_abundance_bio_posterior_rpk20221017.RDS")) %>% 
   filter(creek != "4Pad5") %>%  #omit upstream of Padden 5, for symmetry w other creeks
   filter(creek != "2Brn") %>%   #omit Barnes because we have few datapoints for that creek in terms of abs concentration
   #filter(species != "Oncorhynchus tshawytscha") %>%  #for now, filter out as rare
@@ -25,20 +26,25 @@ d$creek_idx <- match(d$creek, unique(d$creek))
 d$time_idx <- match(d$time, unique(d$time))
 d$species_idx <- match(d$species, unique(d$species))
 
-#insert dummy data to reflect non-observation at Barnes upstream for cutthroat
-# d <- d %>% 
+# insert dummy data to reflect non-observation at Barnes upstream for cutthroat
+# d <- d %>%
 #   full_join(expand_grid(creek ="2Brn",
 #                         creek_idx = 2,
 #                         station_idx = 2,
 #                         species_idx = 1,
 #                         time_idx = 1:length(unique(d$time_idx)),
 #                         meandnaconc = 0.01))
-
+# 
 
 
 # create duplicate object, useful for filtering to subset, etc
 f <- d %>% arrange(time_idx, creek_idx) %>% 
   ungroup()
+
+# add construction index
+construction <- which(f$creek_idx == 3 & f$time_idx %in% c(7:12))
+  f$construction_idx <- 1
+  f$construction_idx[construction] <- 2
 
 #find elements to treat as missing data
 missing <- f %>% 
@@ -63,10 +69,14 @@ stan_data <- list(
   Ncreek = length(unique(f$creek_idx)),
   Nstations = length(unique(f$station_idx)),
   Nspecies = length(unique(f$species_idx)),
+  Nconstruction = length(unique(f$construction_idx)),
+  MinconstructionTimepoint = f[f$construction_idx==2,]$time_idx %>% min(),
+  NconstructionTimepoints = f[f$construction_idx==2,]$time_idx %>% unique() %>% length(),
   time_idx = f$time_idx,
   creek_idx = f$creek_idx,
   station_idx = f$station_idx,
   species_idx = f$species_idx,
+  construction_idx = f$construction_idx,
   y_logeDNA = log(f$meandnaconc),
   N_unobserved = nrow(missing),
   unobserved_time_idx= as.array(missing$time_idx),
@@ -75,24 +85,9 @@ stan_data <- list(
   unobserved_species_idx = as.array(missing$species_idx)
 )
 
-#######RUN Stan Models#######
-#here, one model with multiple betas per time point (one for each creek) and one model with a single beta across creeks in a time point
+#######RUN Stan Model#######
 
-# stanMod_multiBeta = stan(file = here("Scripts/timeseries_model/timeSeries_multispecies4.stan") ,data = stan_data,
-#                          verbose = FALSE, chains = 3, thin = 1,
-#                          warmup = 500, iter = 1000,
-#                          control = list(adapt_init_buffer = 175,
-#                                         max_treedepth=12,
-#                                         stepsize=0.01,
-#                                         adapt_delta=0.7,
-#                                         metric="diag_e"),
-#                          #pars = stan_pars,
-#                          refresh = 10,
-#                          boost_lib = NULL,
-#                          init = 0,
-#                          #sample_file = "temp/tmp.csv"
-# )
-stanMod_oneBeta = stan(file = here("Scripts/timeseries_model/timeSeries_oneBeta_phi3.stan") ,data = stan_data,
+stanMod = stan(file = here("Scripts/timeseries_model/timeSeries_20221028.stan") ,data = stan_data,
                        verbose = FALSE, chains = 3, thin = 1,
                        warmup = 500, iter = 1500,
                        control = list(adapt_init_buffer = 175,
@@ -107,41 +102,33 @@ stanMod_oneBeta = stan(file = here("Scripts/timeseries_model/timeSeries_oneBeta_
                        #init = stan_init_f2(n.chain=N_CHAIN,N_species=N_species),
                        #sample_file = "temp/tmp.csv"
 )
-# stanMod_phi = stan(file = here("Scripts/timeseries_model/timeSeries_oneBeta_phi.stan") ,data = stan_data,
-#                        verbose = FALSE, chains = 3, thin = 1,
-#                        warmup = 500, iter = 1000,
-#                        control = list(adapt_init_buffer = 175,
-#                                       max_treedepth=12,
-#                                       stepsize=0.01,
-#                                       adapt_delta=0.7,
-#                                       metric="diag_e"),
-#                        init = 0,
-#                        #pars = stan_pars,
-#                        refresh = 10,
-#                        boost_lib = NULL
-#                        #init = stan_init_f2(n.chain=N_CHAIN,N_species=N_species),
-#                        #sample_file = "temp/tmp.csv"
-# )
 
-#model selection
-# loo::loo_compare(loo(stanMod_multiBeta),
-#                  loo(stanMod_oneBeta))
-#we prefer the simpler model, slightly
-
-plot(stanMod_oneBeta, par = c("mu"))
-plot(stanMod_oneBeta, par = c("sigma_eta", "sigma_dna"))
-#plot(stanMod_oneBeta, par = c("eta"))
-plot(stanMod_oneBeta, par = c("beta_1"))
-plot(stanMod_oneBeta, par = c("phi"))
-
+# plot(stanMod, par = c("mu"))
+# plot(stanMod, par = c("sigma_eta", "sigma_dna"))
+# #plot(stanMod, par = c("eta"))
+# plot(stanMod, par = c("beta_1"))
+# plot(stanMod, par = c("alpha"))
+# plot(stanMod, par = c("gamma"))
+# traceplot(stanMod, par = c("gamma[1,1,1,7]",
+#                            "gamma[2,1,1,7]",
+#                            "gamma[1,1,2,5]",
+#                            "gamma[2,1,2,7]"
+#                            ))
 # 
-# traceplot(stanMod_oneBeta, par = c("sigma_eta", "sigma_dna"))
+#gamma[station, species, construction, time]
+summary(stanMod, par = "gamma")$summary[,1] %>% sort() %>% head()
+summary(stanMod, par = "gamma")$summary[,"Rhat"] %>% sort() %>% tail()
+summary(stanMod)$summary[,"Rhat"] %>% sort() %>% tail()
+#shinystan::launch_shinystan(stanMod)
+# drop_parameters(as.shinystan(stanMod), pars = c("eta", "alpha", "mu", "mu_0")) %>% launch_shinystan()
+# 
+# traceplot(stanMod, par = c("sigma_eta", "sigma_dna"))
 # ############################
 
 # Save, if desired
-# saveRDS(list(stan_data,
-#              stanMod_oneBeta,
-#              f), file = "modelFit_20221011.RDS")
+saveRDS(list(stan_data,
+             stanMod,
+             f), file = "modelFit_20221028.RDS")
 
 
 
